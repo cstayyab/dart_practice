7392692dfe7fa22343660ae4bcb09a0256c7c9aa
Muhammad Tayyab Sheikh <cstayyab@gmail.com>
2020-11-27 09:30:14 +0000

    Testing Point Class

--- bin/dart_practice.dart
+++ bin/dart_practice.dart
@@ -1,5 +1,8 @@
 import 'package:dart_practice/dart_practice.dart' as dart_practice;
+import 'package:dart_practice/point.dart';
 
 void main(List<String> arguments) {
   print('Hello world: ${dart_practice.calculate()}!');
+  var p = Point.fromJSON({'x': 3, 'y': 4});
+  print('(3,4) is ${p.distanceFromOrigin} units away from origin.');
 }
